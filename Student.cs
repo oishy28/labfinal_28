b170fdea692ab1956b4c0eddb050fde413aa9b34
oishy28 <95131852+oishy28@users.noreply.github.com>
2022-03-22 10:42:42 -0700

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labfinal_even
+{
+    public class Student
+    {
+        public string Id;
+        public string Name;
+        public int attendence;
+        public int quiz1;
+        public int quiz2;
+        public int quiz3;
+        public int quiz4;
+        public int mid;
+        public int final;
+        public int viva;
+        public int percentage;
+        public int total_quiz;
+        public string grade;
+       
+
+
+    }
+}
---
+++ StudentList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labfinal_even
+{
+    public class StudentList
+    {
+        public static List<Student> studentList = new List<Student>();
+    }
+}
